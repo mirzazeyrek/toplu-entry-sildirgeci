d44eeab892c2739b3d21736c94df39ece996f3f4
mirzazeyrek <github@mersinuni.net>
2016-03-14 00:46:56 +0200

    Update index.php
    *yeni kısıtlamalar ve tasarıma ilişkin güncellemeler eklendi. 
    **daha açıklayıcı bilgilendirme mesajları.

--- index.php
+++ index.php
@@ -11,8 +11,8 @@ require("include/require.php");
 parse_str(implode('&', array_slice($argv, 1)), $_GET);
 
 if(!$_GET['sifre'] or !$_GET['kullanici']) {
- echo " kullanıcı adı ve şifre girmeyi unuttunuz. örnek giriş: \n 'php index.php sifre=1234 kullanici=ssg' \n";
- die;
+    echo " kullanıcı adı ve şifre girmeyi unuttunuz. örnek giriş: \n 'php index.php sifre=1234 kullanici=ssg' \n";
+    die;
 }
 
 /*
@@ -28,19 +28,17 @@ $eksisozluk = new Eksisozluk();
 $eksisozluk->login($_GET['kullanici'],$_GET['sifre']);
 $eksisozluk->ben();
 $eksisozluk->son_entryleri();
-
 while(count($eksisozluk->getSonEntryleri())>0) {
     foreach($eksisozluk->getSonEntryleri() as $key=>$entry) {
-        $eksisozluk->sil($entry["id"]);
+        $eksisozluk->sil($entry["href"]);
+        echo "\n bir dürüm yeme süresi* kadar bekletiyorum. ";
+        echo "\n *15 saniyeye tekabül etmektedir. \n";
+        sleep(15);
+        // dürümsel sebeplerle bu beklemeyi yapmak zorundayız maalesef!!!
+
     }
     $eksisozluk->son_entryleri();
 }
 
-echo "\n sildik gitti hepsi çöpte şimdi :( \n";
-
-
-
-
-
-
-
+echo "\n veda zamanı gelirken teletabilerin :( \n";
+echo "\n ebesini sikeyim dürümcü pezevenklerin :) \n"; 
